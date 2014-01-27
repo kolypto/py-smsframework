c21f1bc871acbbd253c1d20e05794eefe9f17989
Mark Vartanyan <kolypto@gmail.com>
2014-01-27 18:20:38 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    # http://pythonhosted.org/setuptools/setuptools.html
+    name='smsframework',
+    version='0.0.1',
+    author='Mark Vartanyan',
+    author_email='kolypto@gmail.com',
+
+    url='https://github.com/kolypto/py-smsframework',
+    license='MIT',
+    description='SMS framework with pluggable providers',
+    long_description=open('README.rst').read(),
+    keywords=['sms', 'message', 'notification', 'receive', 'send'],
+
+    packages=['smsframework'],
+    scripts=[],
+
+    install_requires=[
+    ],
+    include_package_data=True,
+
+    platforms='any',
+    classifiers=[
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        #'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ],
+)
