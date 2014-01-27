3b7b0cf8edc12893e035abd42b3aa3e4e1418c28
Mark Vartanyan <kolypto@gmail.com>
2014-01-28 01:00:14 +0200

    data: refactoring for IncomingMessage, OutgoingMessage

--- smsframework/data/OutgoingMessage.py
+++ smsframework/data/OutgoingMessage.py
@@ -1,8 +1,14 @@
+from .OutgoingMessageOptions import OutgoingMessageOptions
+
+
 class OutgoingMessage(object):
     """ Outgoing Message: Mobile Terminated (MT)
 
         Represents a message that's being sent or was sent to the provider
     """
+
+    #: Routing values
+    routing = None
 
     #: Unique message id, populated by the provider on send
     msgid = None
@@ -10,7 +16,7 @@ class OutgoingMessage(object):
     #: Provider-dependent message info dict, populated by the provider on send
     meta = None
 
-    def __init__(self, dst, body, src=None, provider=None, route=None):
+    def __init__(self, dst, body, src=None, provider=None):
         """ Create a message for sending
 
             :type dst: str | None
@@ -22,51 +28,54 @@ class OutgoingMessage(object):
             :param src: Source phone number, digits only
             :type provider: str | None
             :param provider: Provider name to use for sending.
-                If not specified explicitly, the message will be routed by the :class:`smsframework.Gateway`,
-                which by default selects the first available provider.
-            :type route: list | tuple | None
-            :param route: Routing values for the message: are used by the routing method
+                If not specified explicitly, the message will be routed using the routing values:
+                see :meth:`OutgoingMessage.route`
         """
         self.src = src
         self.dst = dst
         self.body = body
 
         self.provider = provider
-        self.route = route
 
         #: Sending options for the Gateway
-        self.options = OutgoingMessageOptions()
+        self.provider_options = OutgoingMessageOptions()
 
         #: Provider-dependent sending parameters
-        self.params = {}
+        self.provider_params = {}
 
     def options(self, **kwargs):
-        """ Specify sending options for the Gateway
+        """ Specify sending options for the Gateway.
+
+            See: :class:`OutgoingMessageOptions`
 
             :param allow_reply: Replies allowed?
             :param status_report: Request a status report from the network?
             :param expires: Message validity period, minutes
             :param senderId: Sender ID to replace the number
             :param escalate: Is a high-pri message? These are delivered faster and costier.
+
+            :rtype: OutgoingMessage
         """
-        self.options.__dict__.update(kwargs)
+        self.provider_options.__dict__.update(kwargs)
         return self
 
+    def params(self, **params):
+        """ Specify provider-specific sending parameters
 
-class OutgoingMessageOptions(object):
-    """ Sending Options for :class:`OutgoingMessage` """
+            :rtype: OutgoingMessage
+        """
+        self.provider_params = params
+        return self
 
-    #: Replies allowed?
-    allow_reply = False
+    def route(self, *args):
+        """ Specify arbitrary routing values.
 
-    #: Request a status report from the network?
-    status_report = False
+            These are used by the Gateway's routing function to decide on which provider to use for the message
+            (if no provider was explicitly specified),
 
-    #: Message validity period, minutes
-    expires = None
+            If no routing values are provided at all - the default route is used.
 
-    #: Sender ID to replace the number
-    senderId = None
-
-    #: Is a high-pri message? These are delivered faster and costier.
-    escalate = False
+            :rtype: OutgoingMessage
+        """
+        self.routing = args
+        return self
---
+++ smsframework/data/OutgoingMessageOptions.py
@@ -0,0 +1,17 @@
+class OutgoingMessageOptions(object):
+    """ Sending Options for :class:`OutgoingMessage` """
+
+    #: Replies allowed?
+    allow_reply = False
+
+    #: Request a status report from the network?
+    status_report = False
+
+    #: Message validity period, minutes
+    expires = None
+
+    #: Sender ID to replace the number
+    senderId = None
+
+    #: Is a high-pri message? These are delivered faster and costier.
+    escalate = False
